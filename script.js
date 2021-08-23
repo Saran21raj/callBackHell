704c31ad6e45c8520d63834967cec981300d5a74
Saran21raj <85423931+Saran21raj@users.noreply.github.com>
2021-08-23 16:38:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+setTimeout(function timer(){
+    const br=document.querySelector('#sr');
+    br.innerText='10';
+    setTimeout(function timer(){
+        const br=document.querySelector('#sr');
+        br.innerText='9';
+        setTimeout(function timer(){
+            const br=document.querySelector('#sr');
+            br.innerText='8';
+            setTimeout(function timer(){
+                const br=document.querySelector('#sr');
+                br.innerText='7';
+                setTimeout(function timer(){
+                    const br=document.querySelector('#sr');
+                    br.innerText='6';
+                    setTimeout(function timer(){
+                        const br=document.querySelector('#sr');
+                        br.innerText='5';
+                        setTimeout(function timer(){
+                            const br=document.querySelector('#sr');
+                            br.innerText='4';
+                            setTimeout(function timer(){
+                                const br=document.querySelector('#sr');
+                                br.innerText='3';
+                                setTimeout(function timer(){
+                                    const br=document.querySelector('#sr');
+                                    br.innerText='2';
+                                    setTimeout(function timer(){
+                                        const br=document.querySelector('#sr');
+                                        br.innerText='1';
+                                        setTimeout(function timer(){
+                                            const fl=document.querySelector('#fl');
+                                            fl.setAttribute("src","Flag-India.jpg")
+                                        },1000)
+                                        setTimeout(function timer(){
+                                            const br=document.querySelector('#sr');
+                                            br.innerText='Happy Independence Day';
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000)
